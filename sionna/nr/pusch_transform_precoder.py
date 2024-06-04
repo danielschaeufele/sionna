188445771345c3c763404078d3393712d876aee4
Daniel Schäufele <Daniel.Schaeufele@hhi.fraunhofer.de>
2024-06-04 13:19:45 +0000

    Fix linter issues

--- sionna/nr/pusch_transform_precoder.py
+++ sionna/nr/pusch_transform_precoder.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2021-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
+"""Class definitions for PUSCH transform precoder and deprecoder"""
+
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
 
@@ -11,16 +13,17 @@ def _check_largest_prime_factor_not_larger_then_5(n):
         while n % p == 0:
             n /= p
     if n > 1:
-        raise ValueError("Number of subcarriers shouldn't have a prime factor > 5")
+        raise ValueError(
+            "Number of subcarriers shouldn't have a prime factor > 5")
 
 
 class PUSCHTransformPrecoder(Layer):
     r"""PUSCHTransformPrecoder(num_subcarriers, dtype=tf.complex64, **kwargs)
-    Performs transform precoding of layer mapped symbols as defined in [3GPP38211]_
-    Sec. 6.3.1.4.
+    Performs transform precoding of layer mapped symbols as defined in
+    [3GPP38211]_ Sec. 6.3.1.4.
 
-    The input will be reshaped into blocks of size ``num_subcarriers`` to which the
-    FFT will be applied individually.
+    The input will be reshaped into blocks of size ``num_subcarriers`` to which
+    the FFT will be applied individually.
 
     The class inherits from the Keras layer class and can be used as layer in a
     Keras model.
@@ -28,7 +31,8 @@ class PUSCHTransformPrecoder(Layer):
     Parameters
     ----------
         num_subcarriers: int
-            Number of subcarriers. The largest prime factor must not be larger than 5.
+            Number of subcarriers. The largest prime factor must not be larger
+            than 5.
 
         dtype : One of [tf.complex64, tf.complex128]
             Dtype of inputs and outputs. Defaults to tf.complex64.
@@ -41,7 +45,8 @@ class PUSCHTransformPrecoder(Layer):
     Output
     ------
         : [...,n], tf.complex
-            Tensor containing the sequence of symbols that have been transform precoded.
+            Tensor containing the sequence of symbols that have been transform
+            precoded.
     """
 
     def __init__(self,
@@ -55,18 +60,19 @@ class PUSCHTransformPrecoder(Layer):
     def call(self, y):
         orig_shape = tf.shape(y)
         y_reshaped = tf.reshape(y, [-1, self._num_subcarriers])
-        y_transformed = tf.cast(tf.sqrt(1 / self._num_subcarriers), self._dtype) * tf.signal.fft(y_reshaped)
+        y_transformed = tf.cast(tf.sqrt(1 / self._num_subcarriers),
+                                self._dtype) * tf.signal.fft(y_reshaped)
         y_result = tf.reshape(y_transformed, orig_shape)
         return y_result
 
 
 class PUSCHTransformDeprecoder(Layer):
     r"""PUSCHTransformDeprecoder(num_subcarriers, dtype=tf.complex64, **kwargs)
-    Performs transform deprecoding of layer mapped symbols as defined in [3GPP38211]_
-    Sec. 6.3.1.4.
+    Performs transform deprecoding of layer mapped symbols as defined in
+    [3GPP38211]_ Sec. 6.3.1.4.
 
-    The input will be reshaped into blocks of size ``num_subcarriers`` to which the
-    IFFT will be applied individually.
+    The input will be reshaped into blocks of size ``num_subcarriers`` to which
+    the IFFT will be applied individually.
 
     The class inherits from the Keras layer class and can be used as layer in a
     Keras model.
@@ -74,7 +80,8 @@ class PUSCHTransformDeprecoder(Layer):
     Parameters
     ----------
         num_subcarriers: int
-            Number of subcarriers. The largest prime factor must not be larger than 5.
+            Number of subcarriers. The largest prime factor must not be larger
+            than 5.
 
         dtype : One of [tf.complex64, tf.complex128]
             Dtype of inputs and outputs. Defaults to tf.complex64.
@@ -87,7 +94,8 @@ class PUSCHTransformDeprecoder(Layer):
     Output
     ------
         : [...,n], tf.complex
-            Tensor containing the sequence of symbols before transform precoding.
+            Tensor containing the sequence of symbols before transform
+            precoding.
     """
 
     def __init__(self,
@@ -101,6 +109,7 @@ class PUSCHTransformDeprecoder(Layer):
     def call(self, y):
         orig_shape = tf.shape(y)
         y_reshaped = tf.reshape(y, [-1, self._num_subcarriers])
-        y_transformed = tf.cast(tf.sqrt(float(self._num_subcarriers)), self._dtype) * tf.signal.ifft(y_reshaped)
+        y_transformed = tf.cast(tf.sqrt(float(self._num_subcarriers)),
+                                self._dtype) * tf.signal.ifft(y_reshaped)
         y_result = tf.reshape(y_transformed, orig_shape)
         return y_result
